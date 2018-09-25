02b97091520ade656e7a399fc1a1ad8c23f00996
thevpaliy <vpaliy97@gmail.com>
2018-09-25 17:30:21 -0500

    Update UI a little bit

--- app/static/js/dashboard.js
+++ app/static/js/dashboard.js
@@ -1,65 +1,74 @@
 'use strict';
 
 window.chartColors = {
-	red: 'rgb(255, 99, 132)',
-	orange: 'rgb(255, 159, 64)',
-	yellow: 'rgb(255, 205, 86)',
-	green: 'rgb(75, 192, 192)',
-	blue: 'rgb(54, 162, 235)',
-	purple: 'rgb(153, 102, 255)',
-	grey: 'rgb(201, 203, 207)'
+    red: 'rgb(255, 99, 132)',
+    orange: 'rgb(255, 159, 64)',
+    yellow: 'rgb(255, 205, 86)',
+    green: 'rgb(75, 192, 192)',
+    blue: 'rgb(54, 162, 235)',
+    purple: 'rgb(153, 102, 255)',
+    grey: 'rgb(201, 203, 207)'
 };
 
 function range(start, end) {
-  return new Array(end - start).fill().map((d, i) => i + start);
+    return new Array(end - start).fill().map((d, i) => i + start);
 }
 
 function createGraph(context, dataset) {
-  var axes = [{
-    gridLines: {
-        display: false,
-        drawBorder: false
-    },
-    ticks: {
-      display: false
-    }
-  }];
-
-  var stats = new Chart(context, {
-    type: 'bar',
-    data: {
-        labels: range(1, 31),
-        datasets: [dataset]
-    },
-
-    options: {
-      legend: {
-        display: false
-      },
-
-    scales: {
-      yAxes: axes,
-      xAxes: [{
+    var axes = [{
         gridLines: {
-          display: false,
-          drawBorder: false
+            display: false,
+            drawBorder: false
+        },
+        ticks: {
+            display: false
         }
-      }]
-    }
-  }
- });
+    }];
+
+    var stats = new Chart(context, {
+        type: 'bar',
+        data: {
+            labels: range(1, 31),
+            datasets: [dataset]
+        },
+
+        options: {
+            responsive: true,
+            maintainAspectRatio: false,
+
+            legend: {
+                display: false
+            },
+
+            title: {
+                display: true,
+                text: 'Words Per Minute',
+                fontSize: 32
+            },
+
+            scales: {
+                yAxes: axes,
+                xAxes: [{
+                    gridLines: {
+                        display: false,
+                        drawBorder: false
+                    }
+                }]
+            }
+        }
+    });
 
 }
 
 $(document).ready(function() {
-  let context = $('#words')[0].getContext('2d');
-  let dataset = {
-    label: 'Words Per Minute',
-    data: range(30, 60),
-    backgroundColor: window.chartColors.purple,
-    borderColor: window.chartColors.purple,
-    borderWidth: 1
-  };
+    let context = $('#words')[0].getContext('2d');
+    let dataset = {
+        label: 'Words Per Minute',
+        data: [30, 32, 31, 30, 35, 35, 24, 12, 43, 30, 32, 31, 30, 35, 35, 24, 12, 43],
+        backgroundColor: window.chartColors.purple,
+        borderColor: window.chartColors.purple,
+        borderWidth: 1
+    };
 
-  createGraph(context, dataset);
+    createGraph(context, dataset);
 });
